0a3853e0d144aa99b60b8b93b48867f401e04fe8
Canek39 <canek-boss@yandex.ru>
2021-07-06 18:57:15 +0200

    les-1

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ const goods = [
 
 
 const $goodsList = document.querySelector('.goods-list');
-  
+
 const renderGoodsItem = ({ title, price }) => {
     return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
 };
-  
+
 const renderGoodsList = (list = goods) => {
     let goodsList = list.map(
-            item => renderGoodsItem(item)
-        );
+        item => renderGoodsItem(item)
+    ).join('\n');
 
     $goodsList.insertAdjacentHTML('beforeend', goodsList);
 }
-  
+
 renderGoodsList();
